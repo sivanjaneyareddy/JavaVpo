e33a0cb017812a4dc758fdf91c56c2dafd9c557b
Pranay Shavili <Pranay.Shavili@realogy.com>
2022-02-28 15:59:42 +0000

    Updated docker.sh

--- Pipeline/Docker-App/docker.sh
+++ Pipeline/Docker-App/docker.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo systemctl daemon-reload #THIS IS RESCUE COMMAND…
-sudo systemctl restart docker
-sudo systemctl status docker
-
 set -ex
 
 docker login -u rohitshavili -p anglo@123
